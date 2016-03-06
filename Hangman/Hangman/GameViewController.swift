b9fce807b12501a5d1ad5f51cd990216f46b1197
zzeleznick <zzeleznick@gmail.com>
2016-03-06 15:42:57 -0800

    added all the buttons

--- Hangman/Hangman/GameViewController.swift
+++ Hangman/Hangman/GameViewController.swift
@@ -10,7 +10,11 @@ import UIKit
 
 class GameViewController: UIViewController {
     
-    let vowels = ["a", "e", "i", "o", "u", "y"]
+    let vowels   = ["a","e","i","o","u","y"]
+    let commons1 = ["b","c","d","g","h","k","l"]
+    let commons2 = ["m","n","p","r","s","t","v"]
+    let uncommon = ["f","j","q","w","x","z"]
+    
     var vowelButtons: [UIButton!]!
     var button2: UIButton!
     var buttonOn: Bool = false
@@ -30,6 +34,8 @@ class GameViewController: UIViewController {
         let phrase = hangmanPhrases.getRandomPhrase()
         print(phrase)
         makeVowelButtons()
+        makeCommonButtons()
+        makeUncommonButtons()
     }
 
     override func didReceiveMemoryWarning() {
@@ -37,58 +43,59 @@ class GameViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    func makeButtons(content: [String], bottom: CGFloat) {
-        for i in 1...content.count-1 {
+    func makeButtons(content: [String], margin: CGFloat, bottom: CGFloat, width: CGFloat = 45.0) {
+        for i in 0...content.count-1 {
             let button: UIButton = UIButton(type: UIButtonType.System)
             button.setTitle(content[i], forState: UIControlState.Normal)
-            button.setBackgroundImage(UIImage(named: "BlueButton"), forState: UIControlState.Normal)
+            button.backgroundColor = UIColor.init(red: 0, green: 0.2, blue: 0.8, alpha: 1)
+            button.layer.cornerRadius = 10
+            button.layer.borderWidth = 0
+            button.layer.borderColor = UIColor.blackColor().CGColor
+            // button.setBackgroundImage(UIImage(named: "BlueButton"), forState: UIControlState.Normal)
+            
             button.titleLabel?.font = UIFont.systemFontOfSize(12)
             button.setTitleColor(UIColor.whiteColor(), forState: UIControlState.Normal)
+            
+            // Add buttons to container
             letterContainer.addSubview(button)
             
             // Disable Autoresizing constraints
             button.translatesAutoresizingMaskIntoConstraints = false
-            let left: CGFloat = -60.0 + 70.0 * CGFloat(i)
+            let space: CGFloat = width + margin
+            let left: CGFloat = 5 + space * CGFloat(i)
+            
             // Auto Layout Rules
             let leftEdge = NSLayoutConstraint(item: button, attribute: NSLayoutAttribute.LeadingMargin, relatedBy: NSLayoutRelation.Equal, toItem: letterContainer, attribute: NSLayoutAttribute.LeadingMargin, multiplier: 1.0, constant: left)
             let bottomConstraint = NSLayoutConstraint(item: button, attribute: NSLayoutAttribute.Bottom, relatedBy: NSLayoutRelation.Equal, toItem: letterContainer, attribute: NSLayoutAttribute.BottomMargin, multiplier: 1.0, constant: bottom)
             
             let heightConstraint = NSLayoutConstraint(item: button, attribute: NSLayoutAttribute.Height, relatedBy: NSLayoutRelation.Equal, toItem: nil, attribute: NSLayoutAttribute.NotAnAttribute, multiplier: 1, constant: 40)
             
-            let widthConstraint  = NSLayoutConstraint(item: button, attribute: NSLayoutAttribute.Width, relatedBy: NSLayoutRelation.Equal, toItem: nil, attribute: NSLayoutAttribute.NotAnAttribute, multiplier: 1, constant: 50)
+            let widthConstraint  = NSLayoutConstraint(item: button, attribute: NSLayoutAttribute.Width, relatedBy: NSLayoutRelation.Equal, toItem: nil, attribute: NSLayoutAttribute.NotAnAttribute, multiplier: 1, constant: width)
             
-            // add all constraints
+            // Add all constraints
             letterContainer.addConstraints([leftEdge, bottomConstraint, heightConstraint, widthConstraint])
         }
     }
     
     func makeVowelButtons() {
-        vowelButtons = []
-        for i in 1...vowels.count-1 {
-            let button: UIButton = UIButton(type: UIButtonType.System)
-            button.setTitle(vowels[i], forState: UIControlState.Normal)
-            button.setBackgroundImage(UIImage(named: "BlueButton"), forState: UIControlState.Normal)
-            button.titleLabel?.font = UIFont.systemFontOfSize(12)
-            button.setTitleColor(UIColor.whiteColor(), forState: UIControlState.Normal)
-            letterContainer.addSubview(button)
-            
-            // Disable Autoresizing constraints
-            button.translatesAutoresizingMaskIntoConstraints = false
-            let left: CGFloat = -60.0 + 70.0 * CGFloat(i)
-            let bottom: CGFloat = -220
-            // Auto Layout Rules
-            let leftEdge = NSLayoutConstraint(item: button, attribute: NSLayoutAttribute.LeadingMargin, relatedBy: NSLayoutRelation.Equal, toItem: letterContainer, attribute: NSLayoutAttribute.LeadingMargin, multiplier: 1.0, constant: left )
-            let bottomConstraint = NSLayoutConstraint(item: button, attribute: NSLayoutAttribute.Bottom, relatedBy: NSLayoutRelation.Equal, toItem: letterContainer, attribute: NSLayoutAttribute.BottomMargin, multiplier: 1.0, constant: bottom)
-            
-            let heightConstraint = NSLayoutConstraint(item: button, attribute: NSLayoutAttribute.Height, relatedBy: NSLayoutRelation.Equal, toItem: nil, attribute: NSLayoutAttribute.NotAnAttribute, multiplier: 1, constant: 40)
-        
-            let widthConstraint  = NSLayoutConstraint(item: button, attribute: NSLayoutAttribute.Width, relatedBy: NSLayoutRelation.Equal, toItem: nil, attribute: NSLayoutAttribute.NotAnAttribute, multiplier: 1, constant: 60)
-            
-            // add all constraints
-            letterContainer.addConstraints([leftEdge, bottomConstraint, heightConstraint, widthConstraint])
-            
-            vowelButtons.append(button)
-        }
+        let margin: CGFloat = 10
+        let bottom: CGFloat = -220
+        makeButtons(vowels, margin: margin, bottom: bottom)
+    }
+    
+    func makeCommonButtons() {
+        let margin: CGFloat = 6
+        let width: CGFloat = 40
+        let bottom1: CGFloat = -140
+        let bottom2: CGFloat = -95
+        makeButtons(commons1, margin: margin, bottom: bottom1, width: width)
+        makeButtons(commons2, margin: margin, bottom: bottom2, width: width)
+    }
+    
+    func makeUncommonButtons() {
+        let margin: CGFloat = 10
+        let bottom: CGFloat = -20
+        makeButtons(uncommon, margin: margin, bottom: bottom)
     }
     
     
